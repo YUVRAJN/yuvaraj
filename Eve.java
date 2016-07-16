28dccaa3cac4f2508ce09429f9f76a18389b4395
YUVRAJN <buddhiyuvraj11@gmail.com>
2016-07-16 04:26:26 -0700

    Add files via upload

---
+++ Eve.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eve;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author yuvaraj
+ */
+
+
+ 
+ public class eve
+         {
+     public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+         int n = sc.nextInt();
+     
+         
+         if(n%2==0)
+        {
+            System.out.println("the no is even");
+        }
+        else
+        {
+            System.out.println("the no is odd");
+        }
+    }
+     }
