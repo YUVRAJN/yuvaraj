060490b23b5003e4161b17d5c2314cead5a716e5
YUVRAJN <buddhiyuvraj11@gmail.com>
2016-07-18 02:47:55 -0700

    Add files via upload

---
+++ Guviii.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guvi;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author yuvaraj
+ */
+public class Guviii {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        Scanner sc=new Scanner(System.in);
+       int i=sc.nextInt();
+        {
+        if(i%2==0)
+            System.out.println("the number is even");
+         else
+        System.out.println("the number is odd");
+    
+                
+                
+                 
+                }
+        // TODO code application logic here
+    }
+}
