9cc59a4b6a9fd81144feaee6fe0d33428cf5554c
YUVRAJN <buddhiyuvraj11@gmail.com>
2016-07-22 03:06:19 -0700

    Add files via upload

---
+++ July22.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package july22;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author yuvaraj
+ */
+public class July22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        String s2=sc.nextLine();
+        int a=Integer.parseInt(s1);
+        int b=Integer.parseInt(s2);
+        System.out.println(a+b);
+        
+        
+        
+        // TODO code application logic here
+    }
+}
